0fe723ff360808e3d1405d37f5d128c84eca1157
Salsa <vova.goncharenko@ekreative.com>
2016-01-13 13:24:50 +0200

    Added sort by Tag

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Tag;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -12,30 +13,55 @@ use AppBundle\Entity\Post;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/{tag}", name="homepage", defaults={"tag" = 0})
      * @Template()
+     * @param string $tag
      * @return Post
      */
-    public function mainAction(Request $request)
+    public function mainAction($tag, Request $request)
     {
         $repository = $this->getDoctrine();
-        $query = $repository->getRepository('AppBundle:Post')->findAll();
+        $tags = $repository->getRepository('AppBundle:Tag')->findAll();
+        if($tag == '0'){
+            $query = $repository->getRepository('AppBundle:Post')->findAll();
+        }else{
+
+            /**
+             * @var \AppBundle\Entity\Tag $tagView
+             */
+            $tagView = $repository->getRepository('AppBundle:Tag')->findOneByName($tag);
+            $query = $tagView->getPosts();
+        }
+
         $paginator  = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $query, /* query NOT result */
             $request->query->getInt('page', 1)/*page number*/,
             5/*limit per page*/
         );
-        return ['posts' => $pagination];
+        return ['posts' => $pagination, 'tags' => $tags, 'tag'=> $tag];
     }
 
     /**
-     * @Route("/infinity-scroll", name="infinity-scroll")
+     * @Route("/infinity-scroll/scroll/{tag}", name="infinity-scroll", defaults={"tag" = 0})
+     * @param Tag $tag
+     * @return array
      */
-    public function fffAction(Request $request)
+    public function fffAction($tag, Request $request)
     {
         $repository = $this->getDoctrine();
-        $query = $repository->getRepository('AppBundle:Post')->findAll();
+        $tags = $repository->getRepository('AppBundle:Tag')->findAll();
+        if($tag == '0'){
+            $query = $repository->getRepository('AppBundle:Post')->findAll();
+        }else{
+
+            /**
+             * @var \AppBundle\Entity\Tag $tagView
+             */
+            $tagView = $repository->getRepository('AppBundle:Tag')->findOneByName($tag);
+            $query = $tagView->getPosts();
+        }
+
         $paginator  = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $query, /* query NOT result */
--- web-src/js/main.js
+++ web-src/js/main.js
@@ -1,40 +1,4 @@
 $(document).ready(function(){
-
-    var page = 2;
-    var total = $('.count').text();
-    var itemsPerPage = 5;
-    $(window).scroll(function () {
-        if($(window).scrollTop() + $(window).height() == $(document).height()) {
-            page++;
-            if((page-1)* itemsPerPage > total) {
-                $('#no-more').show();
-                $('#more').hide();
-            }else{
-                $('#more').show();
-
-                $.get("/app_dev.php/infinity-scroll", function(json) {
-                    console.log(json);
-                    for (var i in json.posts) {
-                        $('.post-outer').html($('.post-outer').html() +
-                            "<div class=\"post hentry\"><div class=\"post-body entry-content\" id=\"\"><div class=\"thumbs\">" +
-                            "<a href=\"post/"+ json.posts[i].slug +"\">"+
-                            "<img src=\"http://4.bp.blogspot.com/-GteVT4IofuQ/VQNd8fCd93I/AAAAAAAADgQ/EOXamheeLDU/s370-c/keyboard-old-typewriter-3319.jpg\" alt=\"Wypas cookie caramels soufflé caramels cookie \">" +
-                            "</a></div>" +
-                            "<div class=\"items-right\">" +
-                            "<h3 class=\"post-title entry-title\">" +
-                            "<a href=\"post/"+ json.posts[i].slug +"\">" + json.posts[i].title + "</a></h3>" +
-                            "<h2 class=\"date-header\"><span>" + json.posts[i].createTime + "</span></h2>" +
-                            "<p class=\"excerpt\">" + json.posts[i].content + "</p>" +
-                            "<div class=\"jump-link\">" +
-                            "<a href=\"post/"+ json.posts[i].slug +"\">Continue Reading<i class=\"fa fa-caret-right\"></i></a></div>" +
-                            "</div><div style=\"clear: both;\"></div></div></div>");
-                    }
-                }, 'json');
-            }
-        }
-    });
     var heightSideBar = $('#admin-panel').height();
     $('#admin-panel .control-panel').height(heightSideBar);
-
-
 });
