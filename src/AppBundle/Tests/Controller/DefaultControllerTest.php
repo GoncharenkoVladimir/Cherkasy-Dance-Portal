98955f53549a553aba53f9c0a2ffc30a13a719c4
Salsa <vova.goncharenko@ekreative.com>
2016-01-15 16:06:51 +0200

    Tests

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,16 +3,18 @@
 namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+
+    public function testMainPage()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertTrue($client->getResponse()->isSuccessful());
     }
+
+
 }
